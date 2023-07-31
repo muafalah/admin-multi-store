b52c30793c5972017a80016cc7383b0548274b45
muafalah <muh.ifa.amrillah@gmail.com>
2023-07-31 12:46:34 +0700

    feat(component): add store switcher

--- app/(dashboard)/[storeId]/(routes)/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,5 +1,17 @@
-const DashboardPage = () => {
-  return <div>This is a Dashboard</div>;
+import prismadb from "@/lib/prismadb";
+
+interface DashboardPageProps {
+  params: { storeId: string };
+}
+
+const DashboardPage = async ({ params }: DashboardPageProps) => {
+  const store = await prismadb.store.findFirst({
+    where: {
+      id: params.storeId,
+    },
+  });
+
+  return <div>This is a Dashboard Store {store?.name}</div>;
 };
 
 export default DashboardPage;
